90a4324d68db31e196114490a36086770a86295d
Alexander von Below <Alex@vonBelow.Com>
2013-03-12 18:28:24 +0100

    Update SSLTestDemoTests.m
    Removed Code for clarity

--- SSLTestDemoTests/SSLTestDemoTests.m
+++ SSLTestDemoTests/SSLTestDemoTests.m
@@ -15,20 +15,6 @@
 
 @implementation SSLTestDemoTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
-
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
-
 - (void)testSynchronousSSLDownload
 {
     NSHTTPURLResponse *resp;
